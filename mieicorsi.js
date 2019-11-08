5b61c2d6fb0e3fbc9eeb8f5e901341e0fbdf484f
Patrick Reichert <patrickreichert@MacBook-Pro-di-its.local>
2019-11-08 11:08:45 +0100

    materia+corsi

--- mieicorsi.js
+++ mieicorsi.js
@@ -1,29 +1,61 @@
-const motivoSpesa = document.querySelector('#motivo-spesa')
-const importoSpesa = document.querySelector('#importo-spesa')
-const aggiungiBottone = document.querySelector('#aggiungi-bottone')
-const cancellaBottone = document.querySelector('#cancella-bottone')
-const listaSpesa = document.querySelector('#list-spesa')
+const nomeDelCorso = document.querySelector('#nomeDelCorso')
+const voto = document.querySelector('#voto')
+const bottoneAggiungi = document.querySelector('#bottoneAggiungi')
+const bottoneCancella = document.querySelector('#bottoneCancella')
+const listaCorsiConCosto = document.querySelector('#listaCorsiConCosto')
+const spesaTotaleOutput = document.querySelector('#spesaTotale')
+const mediaOutput = document.querySelector('#media')
 
-const cancellaForm = () => {
-    motivoSpesa.value = ''
-    importoSpesa.value= ''
+const alertController = document.querySelector('ion-alert-controller')
+
+let spesaTotale = 0
+let media = 0
+
+function noNaturalNumber(event) {
+    var charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode == 101 || charCode == 69)
+        return false;
+
+    return true;
 }
 
-cancellaBottone.addEventListener('click', cancellaForm)
-aggiungiBottone.addEventListener('click', () => {
-    const motivoInserito = motivoSpesa.value
-    const importoInserito = importoSpesa.value
+const cancella = () => {
+    nomeDelCorso.value = ''
+    voto.value = ''
+}
+bottoneCancella.addEventListener('click', cancella)
+bottoneAggiungi.addEventListener('click', () => {
 
-    if(importoInserito <= 0 || importoInserito.trim().length <= 0 || motivoInserito.trim().lenght <= 0)
-    {
+    // Prendo i valori dall'html e li metto in una costante
+    const nomeDelCorsoInserito = nomeDelCorso.value
+    const votoInserito = voto.value
 
+    if (
+        votoInserito <= 0
+        || votoInserito > 5
+        || votoInserito.trim().length <= 0
+        || nomeDelCorsoInserito.trim().length <= 0
+    ) {
+        // alert('Valori non validi')
+        alertController.create({
+            message: 'Inserisci solo valori validi, non sono ammese spese a costo zero o materie che non esistono!',
+            header: 'Valore o valori non validi',
+            buttons: ['Ok']
+        }).then(alertElem => {
+            alertElem.present()
+        })
+        cancella()
+        return
     }
-    else
-    {
-        console.log('SPESA: €', importoInserito, motivoInserito)
-        const nuovoElemento = document.createElement('ion-item')
-        nuovoElemento.textContent = motivoInserito + ': € ' + importoInserito
-        listaSpesa.appendChild(nuovoElemento)
-        cancellaForm()
-    }
+
+    console.log('Totale: € ', nomeDelCorsoInserito, votoInserito)
+    const nuovoElem = document.createElement('ion-item')
+    nuovoElem.textContent = nomeDelCorsoInserito + ': € ' + votoInserito
+    listaCorsiConCosto.appendChild(nuovoElem)
+
+    // +variabileStringa la transforma in numero
+    spesaTotale += +votoInserito
+    console.log('Totale', spesaTotale)
+    spesaTotaleOutput.textContent = 'Spesa totale: €' + spesaTotale
+    cancella()
 })
