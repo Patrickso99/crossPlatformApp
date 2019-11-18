9d81bf21e324573a8c738a58d10a036f439cc4b3
Patrick Reichert <patrickreichert@MBP-di-its.homenet.telecomitalia.it>
2019-11-18 11:17:52 +0100

    add map js module

--- src/data.js
+++ src/data.js
@@ -1,52 +1,105 @@
 module.exports = {
-    "calendar": [
+  "calendar": [
+    {
+      "time": "8:00",
+      "sessions": [
         {
-            "time": "8:00",
-            "sessions":
-            [
-                {
-                    "nome": "Colazione",
-                    "inizio": "8:00",
-                    "fine": "9:00",
-                    "luogo": "Caffè degli specchi",
-                    "id": "162gs38dh"
-                },
-                {
-                    "nome": "Presentazione ESOF 2019",
-                    "inizio": "9:00",
-                    "fine": "10:00",
-                    "luogo": "Piazza Unità",
-                    "id": "@263gs79"
-                },
-                {
-                    "nome": "Merenda",
-                    "inizio": "10:00",
-                    "fine": "11:00",
-                    "luogo": "Via Monfort",
-                    "id": "3efwfefw"
-                }
+          "nome": "Colazione",
+          "inizio": "8:00",
+          "fine": "9:00",
+          "luogo": "Caffè degli specchi",
+          "id": "162gs38dh"
+        },
+        {
+          "nome": "Presentazione ESOF 2019",
+          "inizio": "9:00",
+          "fine": "10:00",
+          "luogo": "Piazza Unità d'Italia",
+          "id": "@263gs79"
+        },
+        {
+          "nome": "FINE",
+          "inizio": "10:00",
+          "fine": "11:00",
+          "luogo": "Piazza Unità d'Italia",
+          "id": "@263gs79"
+        }
+      ]
+    }
+  ],
+  "speakers": [
+    {
+      "nome": "Mario Bianchi",
+      "img": "https://www.netclipart.com/pp/m/120-1205442_download-bart-simpson-head-png.png",
+      "intervento": "Colazione",
+      "inizio": "8:00",
+      "fine": "9:00",
+      "desc": "bla bla bla caffè cornetto e amaro",
+      "id": "27sghs$"
+    },
+    {
+      "nome": "Dario Cross",
+      "img": "https://www.film.it/fileadmin/mediafiles/film/multimedia/201707/images/628x400/MV5BYWUyMTI0NTMtYTMyZC00ZjAwLTk0MWUtYWZjNWU4ZDFmZmFmXkEyXkFqcGdeQXVyNTc3MjUzNTI%40._V1_SY1000_CR0%2C0%2C1499%2C1000_AL_.jpg",
+      "intervento": "Presentazione ESOF 2019",
+      "inizio": "9:00",
+      "fine": "10:00",
+      "desc": "bla bla bla caffè cornetto e amaro",
+      "id": "37sshs$"
+    },
+    {
+      "nome": "Franco Tell",
+      "img": "https://i1.wp.com/www.bluegrassyouthballet.org/bybsite/wp-content/uploads/2014/08/Dia-Web-Graphic.jpg",
+      "intervento": "Fine",
+      "inizio": "10:00",
+      "fine": "11:00",
+      "desc": "bla bla bla caffè cornetto e amaro",
+      "id": "77s@hs$"
+    }
+  ],
+  "map": {
+    "geojson": {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "properties": {
+            "evento": "colazione"
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              13.762092590332031,
+              45.649268385921395
             ]
-        }
-    ],
-    "speakers": [
-        {
-            "nome": "Mario Rossi",
-            "img": "https://www.turinhouse.it/wp-content/uploads/2017/01/Adam-Jarod-400x400.jpg",
-            "intervento": "Colazione",
-            "descrizione": "bla bla bla"
+          }
         },
         {
-            "nome": "Luigi Verde",
-            "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNP8rL0bm9gKOHfyP6W6B1g3rSiL651hYwJdh-ivCXKbJM-vQTHA&s",
-            "intervento": "Pranzo",
-            "descrizione": "ble ble ble pizza verde"
+          "type": "Feature",
+          "properties": {
+          "evento": "Inizio"
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              13.759925365447998,
+              45.646283314737836
+            ]
+          }
         },
         {
-            "nome": "Sonic Blu",
-            "img": "https://0.academia-photos.com/3872224/12599680/14017225/s200_mario.rossi_monti.jpg",
-            "intervento": "Cena",
-            "descrizione": "blu blu blu pizza blu"
+          "type": "Feature",
+          "properties": {
+            "evento": "fine"
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              13.764474391937254,
+              45.64815837802974
+            ]
+          }
         }
-
-    ]
+      ]
+    }
+  }
 }
